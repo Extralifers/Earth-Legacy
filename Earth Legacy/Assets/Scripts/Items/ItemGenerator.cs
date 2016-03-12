5b1885f0352de7df92b045e606d80fb901c0581a
baelarkash <alberchis@gmail.com>
2016-03-12 13:47:58 +0100

    Modificación de los generadores de item y nivel

--- Earth Legacy/Assets/Scripts/Items/ItemGenerator.cs
+++ Earth Legacy/Assets/Scripts/Items/ItemGenerator.cs
@@ -5,21 +5,23 @@ using System.IO;
 
 public class ItemGenerator : MonoBehaviour {
 
-    public static List<Item> items = new List<Item>();
+
+	public static List<string> nameItems = new List<string> ();
+    public List<GameObject> items = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
 
         initializePools();
 
-        Item ItemRandom = items[(int)Random.Range(0F, items.Count)];
+        string itemRandom = nameItems[(int)Random.Range(0F, items.Count)];
         //Item ItemRandom = items[0];
         //Debug.Log(ItemRandom.itemName);
 
         //Descubrir como saber la posicion de la habitacion en la que esta
 
-        this.gameObject.AddComponent<Item>();
-        GameObject itemprueba = (GameObject) Instantiate(Resources.Load(ItemRandom.itemName), this.gameObject.transform.position+ new Vector3(0, -3F, 0F), new Quaternion(0, 0, 0, 0));
+
+        items.Add( (GameObject) Instantiate(Resources.Load(itemRandom), this.gameObject.transform.position+ new Vector3(0, -3F, 0F), new Quaternion(0, 0, 0, 0)));
     }
 
     // Update is called once per frame
@@ -37,22 +39,10 @@ public class ItemGenerator : MonoBehaviour {
         {
             
             string[] namefile = file.Name.Split('.');
-            Debug.Log(file.Name);
-            items.Add(new Item("ItemPrefabs/"+namefile[0]));
+			nameItems.Add("ItemPrefabs/"+namefile[0]);
         
 
         }
-        /*foreach (var item in items)
-        {
-            if (item == null)
-            {
-                Debug.Log("Puntero a null en un objeto");
-            }
-            else
-            {
-                Debug.Log(item.ToString());
-            }
 
-        }*/
     }
 }
