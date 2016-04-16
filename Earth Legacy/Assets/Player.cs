a3f94da1f5ad6ffec498b7d7d8dc82778eac0293
Aportillog <aportillog92@gmail.com>
2016-04-16 16:41:40 +0200

    Test Commit

---
+++ Earth Legacy/Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    
+    public class PlayerStats
+    {
+        public int Health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public void DamagePlayer(int damage)
+    {
+        playerStats.Health -= damage;
+        if(playerStats.Health <= 0)
+        {
+            GameMaster.killPlayer(this);
+        }
+    }
+}
